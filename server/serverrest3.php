6aaa64bb8d50e7b9c770485c36562d061de41126
Kania kustiani <kaniakustiani@gmail.com>
2016-04-07 12:47:38 +0700

    Kania Kustiani 1144024

---
+++ server/serverrest3.php
@@ -0,0 +1,32 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+	//$id= $_SERVER['HTTP_ID'];	
+  $json = file_get_contents('php://input');
+  $obj = json_decode('$json');
+  $id=$_GET['id'];
+	$value = "didapatkan " .$id;
+}
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  $json = file_get_contents('php://input');
+	$obj = json_decode($json);
+	$value =  "berhasil diinput ".$obj->{'nama'};
+}
+
+if($_SERVER['REQUEST_METHOD'] == "PUT"){
+	$json = file_get_contents('php://input');
+	$obj = json_decode($json);	
+	//$id= $_SERVER['HTTP_ID'];
+	$value = "berhasil diupdate ".$obj->{'nama'};
+}
+
+if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+	//$id= $_SERVER['HTTP_ID'];	
+	$value = "dihapus ";
+	
+}
+
+header('Content-type: application/json');
+//return JSON array
+exit(json_encode($value));
+?>
